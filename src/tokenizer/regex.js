023fffd9195ddf01973a69a0e92a13ea0d4f1057
domingo2000 <dedwards@buk.cl>
2024-03-17 12:26:24 -0300

    refactor: added comments of common mark compliant regex

--- src/tokenizer/regex.js
+++ src/tokenizer/regex.js
@@ -1,11 +1,13 @@
 
 
 const regex = {
+    // Common mark compliant
+    thematicBreak: /^ {0,3}(?:(?:[\*-_][ \t]*){3,})(?:\n+|$)/,
     atxHeading: /^ {0,3}(#{1,6})[ \t]*(.*?)(?:[ \t]+#+[ \t]*)?(?:\n+|$)/,
     setextHeading: /^((?:.+\n)+)( {0,3}([-=])+[ \t]*)(?:\n+|$)/, // The first group have an extra \n that must be removed in the lexer
+    // Other
     textBlock: /^(.+)(?:\n+|$)/,
     textInline: /^([\w\s\?\!\.\']+)[\n+|$]?/,
-    thematicBreak: /^ {0,3}(?:(?:[\*-_][ \t]*){3,})(?:\n+|$)/,
     bold: /^\*\*(\*?.*?\*?)\*\*/,
     italic: /^\*(.*?)\*/,
     blockquote: /^> {0,}(.*)(?:\n+|$)/
