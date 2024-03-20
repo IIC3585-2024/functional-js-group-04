8f3338d82da6248fa62601f97eb1428e5a3e7ff7
Domingo2000 <40220174+domingo2000@users.noreply.github.com>
2024-03-20 19:59:00 -0300

    fix: inline combined or with punctuation * characters where not parsed compleetly (#3)

--- src/tokenizer/tokenizer.test.js
+++ src/tokenizer/tokenizer.test.js
@@ -59,3 +59,47 @@ describe("blank line", () => {
         expect(tokens).toMatchSnapshot();
     });
 })
+
+describe("inline text", () => {
+    test("bold", () => {
+        const src = '**foo**';
+        const tokens = tokenize(src);
+        expect(tokens).toMatchSnapshot();
+    });
+
+    test("italic", () => {
+        const src = '*foo*';
+        const tokens = tokenize(src);
+        expect(tokens).toMatchSnapshot();
+    });
+
+    test("text_inline", () => {
+        const src = 'foo';
+        const tokens = tokenize(src);
+        expect(tokens).toMatchSnapshot();
+    });
+
+    test("char_inline", () => {
+        const src = '*';
+        const tokens = tokenize(src);
+        expect(tokens).toMatchSnapshot();
+    });
+
+    test("bold and italic", () => {
+        const src = '***foo bar***';
+        const tokens = tokenize(src);
+        expect(tokens).toMatchSnapshot();
+    });
+
+    test("bold and italic and text_inline", () => {
+        const src = '***bold and italic***text';
+        const tokens = tokenize(src);
+        expect(tokens).toMatchSnapshot();
+    });
+
+    test("inline unmatched asterisks", () => {
+        const src = '***abc**';
+        const tokens = tokenize(src);
+        expect(tokens).toMatchSnapshot();
+    });
+})
